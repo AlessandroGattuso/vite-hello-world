a4ee92ba11e95eb2f0605ec3ed6f813de7bd4e50
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-01-10 17:36:27 +0100

    site done

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Header from "./components/Header.vue";
+import Jumbotron from './components/Jumbotron.vue';
+
+export default {
+  components:{
+    Header,
+    Jumbotron
+  }
+}
+</script>
+
+<template>
+  <Header></Header>
+  <Jumbotron></Jumbotron>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script>
+</script>
+<template>
+  <nav class="navbar navbar-light bg-dark d-flex justify-content-center">
+    <div class="container-fluid">
+      <a class="navbar-brand text-light" href="#">
+        <img src="/src/assets/vue.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+        Vue
+      </a>
+    </div>
+  </nav>
+</template>
+<style>
+  
+</style>
