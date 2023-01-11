59df0cf0117fc5defbb16ed76682710770345880
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-01-11 11:08:04 +0100

    navbar fixed

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -9,11 +9,13 @@ export default{
 }
 </script>
 <template>
-  <nav class="navbar navbar-light bg-dark d-flex justify-content-center">
+  <nav class="navbar navbar-light bg-dark d-flex justify-content-between">
     <div class="container-fluid">
       <a class="navbar-brand text-light" href="#">
         <img src="/src/assets/vue.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
         {{title}}
+      </a>
+      <a href="" class="navbar-brand text-light">
         <i class="fa-solid fa-cart-shopping"></i>
       </a>
     </div>
